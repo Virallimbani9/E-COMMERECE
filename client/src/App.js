301adcb82f09f481a099f80573d115cee3e02f9e
Viral Limbani <viral9411@gmail.com>
2024-05-01 18:46:50 +0530

    fornted

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Layout from "./components/Layouts/Layout";
+
+function App() {
+  return (
+    <>
+      <Layout />  
+      <h1>Helloooo</h1>
+    </>
+  );
+}
+
+export default App;
